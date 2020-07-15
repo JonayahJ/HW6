4bfba8d9417104082f61ce33709cc97824cf1956
Jonayah <jonayah@thinkhalcyon.com>
2020-07-14 23:00:15 -0400

    adding in my classes and ids

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-//frontend
+$(document).ready(function(){
 
-//1. search bar id="userCity" btn id="search"
-//1.1 local storage btn 
+    //frontend
 
-//2. 1 day forcast: 
-//2.1a city 
-//2.1a today's date (moment.js) 
-//2.2 temperature 
-//2.3 humidity 
-//2.4 windspeed 
-//2.5 uv (get lon and lat [colors]), 
-//2.6 icon id="oneDay"
+    //1. search bar id="userCity" btn id="search"
+    //1.1 local storage btn 
 
-//3. 5 day forcast 
-//3.1 date
-//3.2 icon 
-//3.3 temp 
-//3.4 humidity
+    //2. 1 day forcast: 
+    //2.1a city 
+    //2.1a today's date (moment.js) 
+    //2.2 temperature 
+    //2.3 humidity 
+    //2.4 windspeed 
+    //2.5 uv (get lon and lat [colors]), 
+    //2.6 icon id="oneDay"
 
-//4. navbar 
+    //3. 5 day forcast 
+    //3.1 date
+    //3.2 icon 
+    //3.3 temp 
+    //3.4 humidity
 
-//backend
+    //4. navbar 
 
-//1. grab usercity
+    //backend
 
-//2. store/ push array and set to local storage (json.stringify)
+    //1. grab usercity
 
-//3. get localstorage  (json.parse)
+    //2. store/ push array and set to local storage (json.stringify)
 
-//4. dynamically append btn to page (forloop based on array length)
+    //3. get localstorage  (json.parse)
 
-//5. call the 5day and 1 day passing the city
+    //4. dynamically append btn to page (forloop based on array length)
 
-// because I don't want to have to keep writing in the API key
-const appid = "acf26acb44236383cfc7ccf03de5f926";
+    //5. call the 5day and 1 day passing the city
 
-//1day forecast
-function oneday(city){
+    // because I don't want to have to keep writing in the API key
+    const appid = "acf26acb44236383cfc7ccf03de5f926";
+
+    //1day forecast
+    function oneday(city){
     console.log(city)
     //api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
     // add url extension to convert kelvin into imperial (F) "&units=imperial"
@@ -44,17 +46,24 @@ function oneday(city){
 
     console.log(urlOneDay)
     
+
+    
     // getting the data
     $.ajax({
         url: urlOneDay,
         method: "GET"
       }).then(function(response) {
         //cityname 
-        // let userCity
+        // var userCity = $("<div>");
+
         
         // temperature
         const temp = response.main.temp;
         console.log(temp) // in Fahrenheit
+        // creating an element to display temp
+        let domTemp = $("<p>").text(temp)
+        // displaying the temp
+        
         
         //humidity 
         const humidity = response.main.humidity;
@@ -93,32 +102,36 @@ function oneday(city){
   
        
       });
-}
-oneday("Boston");
+    }
+    oneday("Boston");
 
-//fiveday forecast
-function fiveday(city){
-    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+appid+"&units=imperial";
-    console.log(city);
-    console.log(urlFiveDay);
+    //fiveday forecast
+    function fiveday(city){
+        var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+appid+"&units=imperial";
+        console.log(city);
+        console.log(urlFiveDay);
 
-    $.ajax({
-        url: urlFiveDay,
-        method: "GET"
-      }).then(function(response) {
-        //3.1 date
-        // const date
+        $.ajax({
+            url: urlFiveDay,
+            method: "GET"
+        }).then(function(response) {
+            //3.1 date
+            // const date
 
-        //3.2 icon 
-        
+            //3.2 icon 
+            
 
-        //3.3 temp 
-        // const temp
+            //3.3 temp 
+            // const temp
 
-        //3.4 humidity
-        // const humidity
-      });
-}
-fiveday("Boston");
+            //3.4 humidity
+            // const humidity
+        });
+    }
+    fiveday("Boston");
+});
+
+
+
 
 
